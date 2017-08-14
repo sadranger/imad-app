75a528afb4302fa03b923796927b47acb6adb5d5
Řãňjîţh Şhäğåńťï <hell@localhost.localdomain>
2017-08-14 22:44:59 +0530

    profil2

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/profile',function(req,res){
+	res.sendFile(path.join(__dirname,'ui','profile.html'));
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -21,7 +25,7 @@ app.get('/ui/tech.jpg', function (req, res) {
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 80;
+var port = 8080;
 app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+  console.log(`Hey Baby, your app is running on port ${port} <3<3<3`);
 });
